6758ce16648f4009817c223df96533f3aa56b94a
QuangDev-Web <ng.quang.coder.new@gmail.com>
2025-09-04 18:42:39 +0700

    Cài đặt Tailwind 3 vào dự án

--- src/app.tsx
+++ src/app.tsx
@@ -2,4 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(
+    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Hello Electron + Tailwind 3 🚀</h1>
+    </div>
+);
