e2f5608af3b99054305880133290871f73d72fa7
QuangDeveloper <ng.quang.coder.new@gmail.com>
2025-09-04 23:52:13 +0700

    Cài đặt Shadcn vào dự án

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Button } from "./components/ui/Button"
 
 const root = createRoot(document.body);
 root.render(
-    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">Hello Electron + Tailwind 3 🚀</h1>
-    </div>
+  <div className="p-10">
+    <Button variant="default">Default</Button>
+    <Button variant="destructive">Delete</Button>
+  </div>
 );
